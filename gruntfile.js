5b8e730c73bd4b03cfcca70f5ea0f5c3ca4c36b3
Chris Dopuch <chris.dopuch@gmail.com>
2014-12-20 12:33:06 -0600

    grunt now builds requirejs, and also grunt connect

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      files: ['Gruntfile.js', 'www/**/*.js', 'www-built/**/*.js'],
+      files: ['Gruntfile.js', 'www/**/*.js'], // Don't JSHint Uglified files
       options: {
         globals: {
           jQuery: true
@@ -10,14 +10,41 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: ['<%= jshint.files %>'],
-      tasks: ['jshint']
+      jshint: {
+        files: ['<%= jshint.files %>'],
+        tasks: ['jshint']
+      },
+      build: {
+        files: ['**/*'],
+        tasks: ['requirejs']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          base: "www-built",
+          keepalive: true
+        }
+      }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          dir: "www-built",
+          appDir: "www",
+          baseURL: "lib"
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'requirejs']);
 
 };
